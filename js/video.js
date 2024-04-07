1938e0356e7d06843b17d69b0053ecb3147a593c
rajmulherkar1 <87848532+rajmulherkar1@users.noreply.github.com>
2024-04-07 19:28:17 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -4,8 +4,10 @@ window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 	video = document.querySelector("#player1");
 	video.load();
-	video.autoplay = true;
+	video.autoplay = false;
 	video.loop = false;
+	console.log("Autoplay is set to " + video.autoplay)
+	console.log("Looping is set to " + video.loop)
 }
 );
 
